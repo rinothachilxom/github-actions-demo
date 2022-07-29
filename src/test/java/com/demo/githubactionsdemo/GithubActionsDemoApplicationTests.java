011c7cb51a6cd78d3ffada127d48586351bce899
Rino Thachil <108757830+rinothachilxom@users.noreply.github.com>
2022-07-29 11:05:34 +0530

    Added Files

---
+++ src/test/java/com/demo/githubactionsdemo/GithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.githubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
